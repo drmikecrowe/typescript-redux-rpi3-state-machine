3dc0b607af2298afade068ec57170e292493cd04
Mike Crowe <drmikecrowe@gmail.com>
2019-01-21 19:31:13 -0500

    Fixing SVG

--- fsm.js
+++ fsm.js
@@ -1,12 +1,12 @@
 module.exports = {
     start: "Closed",
     transitions: [
-        { name: 'doOpen',  from: 'Closed', to: 'Opened',   dot: { color: 'blue', headport: 'n', tailport: 'n' } },
-        { name: 'doClose', from: 'Opened',   to: 'Closed', dot: { color: 'red',   headport: 's', tailport: 's' } }
+        { name: 'doOpen',  from: 'Closed', to: 'Opened',   dot: { color: 'blue' } },
+        { name: 'doClose', from: 'Opened',   to: 'Closed', dot: { color: 'red' } }
     ],
     statedefs: [ 
-        { name: 'Opened',   dot: { color: 'blue', headport: 'n', tailport: 'n' } },
-        { name: 'Closed', dot: { color: 'red',   headport: 's', tailport: 's' } }
+        { name: 'Opened',   dot: { color: 'blue' } },
+        { name: 'Closed', dot: { color: 'red' } }
     ],
 }
  
