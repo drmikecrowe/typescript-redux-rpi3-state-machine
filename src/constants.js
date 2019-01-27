9ef5e941c76ed5c6d007a17bc6463681056e59f5
Mike Crowe <drmikecrowe@gmail.com>
2019-01-27 16:44:05 -0500

    Removing .js and .js.map files

--- src/constants.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-var InputAdapter;
-(function (InputAdapter) {
-    InputAdapter[InputAdapter["KEYBOARD"] = 0] = "KEYBOARD";
-    InputAdapter[InputAdapter["BUTTONS"] = 1] = "BUTTONS";
-})(InputAdapter = exports.InputAdapter || (exports.InputAdapter = {}));
-exports.InputRouter = InputAdapter.KEYBOARD;
-//# sourceMappingURL=constants.js.map
--- src/store/constants.js
+++
@@ -1,4 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.constants = {};
-//# sourceMappingURL=constants.js.map
